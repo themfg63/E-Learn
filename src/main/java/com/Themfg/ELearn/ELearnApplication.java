b979dd7fb052b59b9327b16ddf5d64dbeaf4c45e
Muhammed Furkan GÜNEŞ <themfg.63@gmail.com>
2024-03-25 10:01:40 +0300

    Proje oluşturuldu

---
+++ src/main/java/com/Themfg/ELearn/ELearnApplication.java
@@ -0,0 +1,13 @@
+package com.Themfg.ELearn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ELearnApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ELearnApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Themfg/ELearn/ELearnApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Themfg.ELearn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ELearnApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
