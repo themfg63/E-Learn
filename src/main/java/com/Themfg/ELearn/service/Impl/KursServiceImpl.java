5bdd86252dd3b918be509539707b75c3100e9d27
Muhammed Furkan GÜNEŞ <themfg.63@gmail.com>
2024-03-27 16:10:18 +0300

    Kurs Service classı oluşturuldu

--- src/main/java/com/Themfg/ELearn/service/Impl/KursServiceImpl.java
+++ src/main/java/com/Themfg/ELearn/service/Impl/KursServiceImpl.java
@@ -1,4 +1,58 @@
 package com.Themfg.ELearn.service.Impl;
 
-public class KursServiceImpl {
+import com.Themfg.ELearn.entity.Kurs;
+import com.Themfg.ELearn.entity.Ogrenci;
+import com.Themfg.ELearn.repository.KursRepository;
+import com.Themfg.ELearn.service.Interface.KursService;
+import com.Themfg.ELearn.service.Interface.OgrenciService;
+import jakarta.transaction.Transactional;
+import org.springframework.data.domain.Sort;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class KursServiceImpl implements KursService {
+    private final KursRepository kursRepository;
+    private final OgrenciService ogrenciService;
+
+    public KursServiceImpl(KursRepository kursRepository,OgrenciService ogrenciService){
+        this.kursRepository=kursRepository;
+        this.ogrenciService=ogrenciService;
+    }
+
+    @Override
+    public List<Kurs> findAllKurs(){
+        return kursRepository.findAll(Sort.by("ad").descending());
+    }
+
+    @Override
+    @Transactional
+    public void kursuKaydet(Kurs kurs){
+        kursRepository.save(kurs);
+    }
+
+    @Override
+    public Optional<Kurs> findKursById(Long id){
+        return kursRepository.findById(id);
+    }
+
+    @Override
+    @Transactional
+    public void kursaYeniOgreniEkle(Kurs kurs, Ogrenci ogrenci)
+    {
+        kurs.ogrenciEkle(ogrenci);
+        kursRepository.save(kurs);
+    }
+
+    @Override
+    @Transactional
+    public void kurstanOgrenciSil(Kurs kurs, Long id){
+        Ogrenci ogrenci = ogrenciService.findOgrenciById(id);
+        kurs.ogrenciSil(ogrenci);
+        kursRepository.save(kurs);
+    }
+
+
 }
--- src/main/java/com/Themfg/ELearn/service/Impl/SinifServiceImpl.java
+++ src/main/java/com/Themfg/ELearn/service/Impl/SinifServiceImpl.java
@@ -1,4 +1,64 @@
 package com.Themfg.ELearn.service.Impl;
 
-public class SinifServiceImpl {
+import com.Themfg.ELearn.entity.Ogrenci;
+import com.Themfg.ELearn.entity.Sinif;
+import com.Themfg.ELearn.repository.SinifRepository;
+import com.Themfg.ELearn.service.Interface.OgrenciService;
+import com.Themfg.ELearn.service.Interface.SinifService;
+import jakarta.transaction.Transactional;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class SinifServiceImpl implements SinifService {
+    private final SinifRepository sinifRepository;
+    private final OgrenciService ogrenciService;
+    public SinifServiceImpl(SinifRepository sinifRepository,OgrenciService ogrenciService){
+        this.ogrenciService=ogrenciService;
+        this.sinifRepository=sinifRepository;
+    }
+
+    @Override
+    public Optional<Sinif> findSinifById(Long id){
+        return sinifRepository.findById(id);
+    }
+
+    @Override
+    @Transactional
+    public void sinifiKaydet(Sinif sinif ){
+        sinifRepository.save(sinif);
+    }
+
+    @Override
+    @Transactional
+    public void sinifiSilById(Long id){
+        sinifRepository.findById(id).ifPresent(sinif -> {
+            for(Ogrenci ogrenci : sinif.getOgrenci1()){
+                ogrenciService.siniftanAyril(ogrenci.getId());
+            }
+            sinifRepository.deleteById(id);
+        });
+    }
+
+    @Override
+    @Transactional
+    public void sinifaOgrenciEkle(Sinif sinif,Ogrenci ogrenci){
+        sinif.ogreniEkle(ogrenci);
+        sinifRepository.save(sinif);
+    }
+
+    @Override
+    public List<Sinif> findAllSinif(){
+        return sinifRepository.findAll();
+    }
+
+    @Override
+    @Transactional
+    public void siniftanOgrenciSil(Sinif sinif,Long id){
+        Ogrenci ogrenci = ogrenciService.findOgrenciById(id);
+        sinif.ogreniSil(ogrenci);
+        sinifRepository.save(sinif);
+    }
 }
